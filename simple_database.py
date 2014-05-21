56b097aab0ca13dcea00eaa0da9ec70fb3ef771c
Jona Haga-Camacho <jona.haga@gmail.com>
2014-05-20 17:38:28 -0700

    Optimized NUMEQUALTO

--- simple_database.py
+++ simple_database.py
@@ -1,14 +1,35 @@
 import sys
 
 database = [{}]
+fq = {}
 
 def level():
     return len(database)-1
+
+def incr_fq():
+    pass
+
+def decr_fq():
+    pass
 
 def end():
     sys.exit(0)
 
 def set_name(name, val):
+    for lvl in range(len(database)-1, -1, -1):
+        if database[lvl]:
+            if name in database[lvl]:
+                prev_val = database[lvl][name]
+                fq[prev_val] -= 1
+                fq[val] = 1
+            else:
+                if val in fq:
+                    fq[val] += 1
+                else:
+                    fq[val] = 1
+        elif lvl == 0:
+            fq[val] = 1
+
     database[level()][name] = val
 
 def get(name):
@@ -21,11 +42,23 @@ def get(name):
 def begin():
     database.append({})
 
+def rb_val(i, lvl):
+    return database[level()-lvl].get(i[0])
+
 def rollback():
     if level() == 0:
         return 'NO TRANSACTION'
-    else:
-        database.remove(database[level()])
+
+    items = database[level()].items()
+    for i in items:
+        if i[1] != 'NULL':
+            fq[i[1]] -= 1    
+        if database[level()-1]:
+            fq[rb_val(i, 1)] += 1
+        elif database[level()-2] and level()-2 >= 0:
+            fq[rb_val(i, 2)] += 1
+
+    database.remove(database[level()])
 
 def commit():
     if level() == 0:
@@ -36,21 +69,14 @@ def commit():
             database.remove(database[lvl])
 
 def unset(name):
+    prev_val = get(name)
+    fq[prev_val] -= 1
     database[level()][name] = "NULL"
 
 def numequalto(val):
-    count = 0
-    i = 0
-
-    for name, value in database[i].iteritems():
-        if value == val:
-            if database[level()].get(name) == 'NULL':
-                count = 0
-            else:
-                count += 1
-                i += 1
-
-    return str(count)
+    if fq.get(val):
+        return fq.get(val)
+    return str(0)
 
 if __name__ == "__main__":
     for line in sys.stdin:
@@ -75,6 +101,3 @@ if __name__ == "__main__":
             sys.stdout.write(str(data) + '\n')
         else:
             pass
-
-
-    print database
